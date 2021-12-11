1bfc83cb23d192a28f7f4b03cc873e6069b3c27f
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-11 18:09:22 +0300

    Added GET for individuals

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-greetings = {
-    "hello": "Hey bud, how are you doing?",
-    "привет": "Здорово, чувак, как делища?" #с русским не работает, надо разибраться с ascii и как его отключить
+items = {
+    '11': {"title": "Неопознанный объект #11", "place": "Мамаев Курган"},
+    '12': {"title": "Неопознанный объект #12", "place": "Мамаев Курган"},
+    '13': {"title": "Неопознанный объект #13", "place": "Мамаев Курган"}
 }
 
-@app.route('/api/v1/greetings/daria/', methods=['GET'])
-def hello():
-    return greetings['hello']
+
+@app.route("/api/v1/individuals/<individual_id>", methods=['GET'])
+def data(individual_id):
+    return items[individual_id]
