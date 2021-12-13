18036ea6eaa795d3627f2f285801c4af74f648e4
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-13 18:17:48 +0300

    Changed the structure of the dictionary -> changed the realisation of GET

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
-items = {
-    '11': {"title": "Неопознанный объект #11", "place": "Мамаев Курган"},
-    '12': {"title": "Неопознанный объект #12", "place": "Мамаев Курган"},
-    '13': {"title": "Неопознанный объект #13", "place": "Мамаев Курган"}
-}
+individuals = [
+    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев Курган"},
+    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев Курган"},
+    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев Курган"}
+]
 
 
-@app.route("/api/v1/individuals/<individual_id>", methods=['GET'])
-def data(individual_id):
-    return items[individual_id]
+@app.route("/api/v1/individuals/<int:individual_id>", methods=['GET'])
+def get_individual(individual_id):
+    individual = filter(lambda x: x['id'] == individual_id, individuals)
+    return next(individual)
+
