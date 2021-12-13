09bc814569717995bd656ef6f92ebc538ad90607
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-13 20:45:46 +0300

    Created a simple PUT-method for individuals

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-items = {
-    '11': {"title": "Неопознанный объект #11", "place": "Мамаев Курган"},
-    '12': {"title": "Неопознанный объект #12", "place": "Мамаев Курган"},
-    '13': {"title": "Неопознанный объект #13", "place": "Мамаев Курган"}
-}
+individuals = [
+    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
+    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
+    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
+]
 
 
-@app.route("/api/v1/individuals/<individual_id>", methods=['GET'])
+@app.route("/api/v1/individuals/<int:individual_id>", methods=['PUT'])
 def data(individual_id):
-    return items[individual_id]
+    individual = next(filter(lambda x: x['id'] == individual_id, individuals))
+    individual['title'] = request.json.get('title', individual['title'])
+    individual['place'] = request.json.get('place', individual['place'])
+    return individual, 201
