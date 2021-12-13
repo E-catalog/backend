35005fa33ee7e2fd8c86f4c4099135c11bb29ee1
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-13 19:17:42 +0300

    Created a simple POST-method for individuals

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
-greetings = {
-    "hello": "Hey bud, how are you doing?",
-    "привет": "Здорово, чувак, как делища?" #с русским не работает, надо разибраться с ascii и как его отключить
-}
+individuals = [
+    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
+    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
+    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
+]
 
-@app.route('/api/v1/greetings/daria/', methods=['GET'])
-def hello():
-    return greetings['hello']
+
+@app.route("/api/v1/individuals/<int:individual_id>", methods=['GET'])
+def get_individual(individual_id):
+    individual = filter(lambda x: x['id'] == individual_id, individuals)
+    return next(individual)
+
+@app.route("/api/v1/individuals", methods=['POST'])
+def create_individual():
+    id = individuals[-1]["id"] + 1
+    new_individual = {
+        "id": id,
+        "title": request.json['title'],
+        "place": request.json['place']
+    }
+    individuals.append(new_individual)
+    return individuals[-1]
