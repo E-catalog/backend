dbe0af7b5cad74918d768d404d0eeb9cde3b0da0
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-14 12:02:01 +0300

    Changed the POST method realisation

--- backend/app.py
+++ backend/app.py
@@ -3,25 +3,20 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-individuals = [
-    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
-    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
-    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
-]
+individuals = {
+    11: {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
+    12: {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
+    13: {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
+}
 
-
-@app.route("/api/v1/individuals/<int:individual_id>", methods=['GET'])
-def get_individual(individual_id):
-    individual = filter(lambda x: x['id'] == individual_id, individuals)
-    return next(individual)
 
 @app.route("/api/v1/individuals", methods=['POST'])
 def create_individual():
-    id = individuals[-1]["id"] + 1
-    new_individual = {
-        "id": id,
+    new_id = max(individuals) + 1
+    individuals[new_id] = {
+        "id": new_id,
         "title": request.json['title'],
         "place": request.json['place']
-    }
-    individuals.append(new_individual)
-    return individuals[-1]
+        }
+
+    return individuals[new_id]
