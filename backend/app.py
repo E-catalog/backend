7ea7bf76e00661d037d67a129b11788a9c1caa80
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-14 12:10:45 +0300

    Changed the PUT method realisation

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-individuals = [
-    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
-    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
-    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
-]
+individuals = {
+    11: {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
+    12: {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
+    13: {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
+}
 
 
 @app.route("/api/v1/individuals/<int:individual_id>", methods=['PUT'])
-def data(individual_id):
-    individual = next(filter(lambda x: x['id'] == individual_id, individuals))
+def update_individual(individual_id):
+    individual = individuals[individual_id]
     individual['title'] = request.json.get('title', individual['title'])
     individual['place'] = request.json.get('place', individual['place'])
     return individual, 201
