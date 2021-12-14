00ac5e4be2d1f02bb01aa09aaaedc0a2d94b6966
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-14 12:15:05 +0300

    Changed the DELETE method realisation

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-individuals = [
-    {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
-    {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
-    {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
-]
+individuals = {
+    11: {"id": 11, "title": "Неопознанный объект #11", "place": "Мамаев курган"},
+    12: {"id": 12, "title": "Неопознанный объект #12", "place": "Мамаев курган"},
+    13: {"id": 13, "title": "Неопознанный объект #13", "place": "Мамаев курган"}
+}
 
 
 @app.route("/api/v1/individuals/<int:individual_id>", methods=['DELETE'])
-def data(individual_id):
-    individual = next(filter(lambda x: x['id'] == individual_id, individuals))
-    individuals.remove(individual)
+def del_individual(individual_id):
+    del individuals[individual_id]
     return jsonify(individuals)
