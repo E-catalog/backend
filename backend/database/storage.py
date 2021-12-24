fcfa899a7db9f6e4a377f9c1ab5c2da13165a1d7
Daria Veselkova <daria.veselkova@yandex.ru>
2021-12-24 16:58:46 +0300

    Added methods in a SqlOndividualsRepo class in storage.py

--- backend/database/storage.py
+++ backend/database/storage.py
@@ -6,20 +6,30 @@ from flask import jsonify
 class SqlIndividualsRepo:
 
     def get_all(self):
-        answer = db_session.query(Individuals).all()
-        result = [str(ind) for ind in answer]
+        all_individuals = db_session.query(Individuals).all()
+        return jsonify(all_individuals)
 
-        return jsonify(result)
+    def get_by_id(self, id: int):
+        individual = db_session.query(Individuals).filter(Individuals.id == id).one()
+        return individual
 
-    def get_by_id(self, id):
-        answer = db_session.query(Individuals).filter(Individuals.id == id)
-        return jsonify(answer)
+    def add(self, new_individual: dict) -> None:
+        individual = Individuals(
+            name=new_individual['name'],
+            place=new_individual['place'],
+            sex=new_individual['sex'],
+            age=new_individual['age'])
+        db_session.add(individual)
+        db_session.commit()
 
-    def add():
-        pass
+    def update(self, id: int, update: dict) -> None:
+        individual = db_session.query(Individuals).filter(Individuals.id == id).one()
+        individual.name = update['name']
+        individual.place = update['place']
+        individual.sex = update['sex']
+        individual.age = update['age']
+        db_session.commit()
 
-    def update():
-        pass
-
-    def delete():
-        pass
+    def delete(self, id: int) -> None:
+        individual = db_session.query(Individuals).filter(Individuals.id == id).one()
+        db_session.delete(individual)
